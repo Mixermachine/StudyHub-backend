20cc3dca8711b34976fa147d3bb7b8b486113dce
Mixermachine <contact@9d3.de>
2019-06-19 08:39:25 +0200

    - Moving sequelize configuration in to .sequelizerc (no need for specific configuration any more

--- config/sequelize_init.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-
-    config: path.join(__dirname, '/config_sequelize.json'),
-    'migrations-path': path.join(__dirname, '../src/migrations'),
-    'seeders-path': path.join(__dirname, '../src/seeders'),
-    'models-path': path.join(__dirname, '../src/models')
-};
