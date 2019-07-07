24b9ee65b361f4a267a8d971f7f4b99f5ecd9434
Mixermachine <dietz1993@gmail.com>
2019-07-08 01:35:27 +0200

    - Fixed: At initiation of genericUserExtension tblObj is overridden by next initiator

--- src/controllers/payee.js
+++ src/controllers/payee.js
@@ -1,10 +1,14 @@
 const generic = require('./genericUserExtension');
 const models = require('../models');
 
-const me = generic.getGeneric(models.Payee);
+const tblObj = models.Payee;
+
+const get = (req, res) => generic.get(tblObj, req, res);
+const post = (req, res) => generic.post(tblObj, req, res);
+const isPayee = (id) => generic.isX(tblObj, id);
 
 module.exports = {
-    get: me.get,
-    post: me.post,
-    isPayee: me.isX
+    get,
+    post,
+    isPayee
 };
