bf4857cb8decd5c207bd10a57119c963d1546f5e
Mixermachine <dietz1993@gmail.com>
2019-07-11 23:43:55 +0200

    - Added environment helper file

---
+++ src/environment.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const devStr = 'development';
+
+const value = process.env.NODE_ENV || devStr;
+
+const isDev = value !== devStr;
+
+const maskMsgIfNotDev = (msg) => isDev() ? msg : undefined;
+
+module.exports = {
+    value,
+    isDev,
+    maskMsgIfNotDev
+};
