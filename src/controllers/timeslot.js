14c1b27d7c29191d9aab5763d2973ce4129ba9c1
Mixermachine <contact@9d3.de>
2019-07-12 13:26:39 +0200

    - Adding punctuation to error messages

--- src/controllers/timeslot.js
+++ src/controllers/timeslot.js
@@ -12,7 +12,7 @@ const get = (req, res) => {
     const studyId = req.params.studyId;
     if (!studyId) {
         return helper.sendJsonResponse(res, 422, "Parameter studyId is missing",
-            "Can't find timeslot without the studyId");
+            "Can't find timeslot without the studyId.");
     }
 
     const timeslotId = req.params.timeslotId;
@@ -24,7 +24,7 @@ const get = (req, res) => {
         .then(study => {
             if (!study) {
                 return helper.sendJsonResponse(res, 404, "Not found",
-                    "Study " + studyId + " does not exist");
+                    "Study " + studyId + " does not exist.");
             }
 
             let queryWhere = {raw: true, attributes: attributes};
@@ -57,7 +57,7 @@ const post = (req, res) => {
     const studyId = req.params.studyId;
     if (!studyId) {
         return helper.sendJsonResponse(res, 422, "Parameter studyId is missing",
-            "Can't post timeslot without the studyId");
+            "Can't post timeslot without the studyId.");
     }
 
     const promises = [];
@@ -91,13 +91,13 @@ const put = (req, res) => {
     const studyId = req.params.studyId;
     if (!studyId) {
         return helper.sendJsonResponse(res, 422, "Parameter studyId is missing",
-            "Can't update timeslot without the studyId");
+            "Can't update timeslot without the studyId.");
     }
 
     const timeslotId = req.params.timeslotId;
     if (!timeslotId) {
         return helper.sendJsonResponse(res, 422, "Parameter timeslotId is missing",
-            "Can't update timeslot without the timeslotId");
+            "Can't update timeslot without the timeslotId.");
     }
 
     models.Study.findByPk(studyId)
@@ -108,13 +108,13 @@ const put = (req, res) => {
             // This could be really bad (creators moving slots to another study so they don't have to pay)
             if (timeslotUpdate.studyId !== undefined) {
                 return helper.sendJsonResponse(res, 403, "Forbidden",
-                    "You can not change the studyId of a timeslot")
+                    "You can not change the studyId of a timeslot.")
             }
 
             // id should never be changed, as references could become invalid
             if (timeslotUpdate.id !== undefined) {
                 return helper.sendJsonResponse(res, 403, "Forbidden",
-                    "You can not change the id of a timeslot")
+                    "You can not change the id of a timeslot.")
             }
 
             // if public user check values first
@@ -122,14 +122,13 @@ const put = (req, res) => {
                 (Object.keys(timeslotUpdate).length !== 1 || timeslotUpdate.participantId !== req.id)) {
                 return helper.sendJsonResponse(res, 401, "Unauthorized",
                     "If you are not the study creator your put should only contain the " +
-                    "field participantId with your own id");
+                    "field participantId with your own id.");
             }
 
             study.getTimeslots({where: {id: timeslotId}})
                 .then(timeslots => {
                     const timeslot = timeslots[0];
                     Object.assign(timeslot, timeslotUpdate);
-
                     timeslot.save()
                         .then(() => res.status(200).send())
                 });
@@ -145,7 +144,7 @@ const generateSecretCheckin = (req, res) => {
 
     if (!studyId || !timeslotId) {
         return helper.sendJsonResponse(res, 422, "Parameter missing",
-            "To generate secretCheckin both studyId and timeslotId are needed");
+            "To generate secretCheckin both studyId and timeslotId are needed.");
     }
 
     models.Study.findByPk(studyId)
@@ -153,7 +152,7 @@ const generateSecretCheckin = (req, res) => {
             if (study) {
                 if (study.creatorId !== req.id) {
                     return helper.sendJsonResponse(res, 401, "Unauthorized",
-                        "Only the creator of the study can generate a secureCheckin");
+                        "Only the creator of the study can generate a secureCheckin.");
                 }
 
                 study.getTimeslots({where: {id: timeslotId}})
@@ -161,13 +160,13 @@ const generateSecretCheckin = (req, res) => {
                         if (timeslots) {
                             if (timeslots[0].participantId === null) {
                                 return helper.sendJsonResponse(res, 405, "Timeslot has no participant",
-                                    "To generate secureCheckin the timeslot has to be assigned to a participant");
+                                    "To generate secureCheckin the timeslot has to be assigned to a participant.");
                             }
 
                             if (timeslots[0].attended) {
                                 return helper.sendJsonResponse(res, 405, "Timeslot has already been attended",
                                     "Student in timeslot has already attended the timeslot. " +
-                                    "Can't create a secureCheckin for an already attended timeslot");
+                                    "Can't create a secureCheckin for an already attended timeslot.");
                             }
 
                             const token = jwt.sign({studyId: studyId, timeslotId: timeslotId}, config.jwtSecret, {
@@ -189,23 +188,23 @@ const secretCheckin = (req, res) => {
 
     if (!studyId || !timeslotId) {
         return helper.sendJsonResponse(res, 422, "Parameter missing",
-            "To perform secretCheckin both studyId and timeslotId are needed");
+            "To perform secretCheckin both studyId and timeslotId are needed.");
     }
 
     jwt.verify(req.params.token, config.jwtSecret, (err, decoded) => {
         if (err) {
             if (err.message === "jwt expired") {
                 return helper.sendJsonResponse(res, 401, "Token expired",
-                    "The token you provided could not be decrypted as it is expired");
+                    "The token you provided could not be decrypted as it is expired.");
             } else {
                 return helper.sendJsonResponse(res, 401, "Failed to decrypt token",
-                    "The token you provided could not be decrypted");
+                    "The token you provided could not be decrypted.");
             }
         }
 
         if (studyId !== decoded.studyId || timeslotId !== decoded.timeslotId) {
             return helper.sendJsonResponse(res, 401, "Token did not match",
-                "The encrypted ids in the token did not match the provided ids in the url");
+                "The encrypted ids in the token did not match the provided ids in the url.");
         }
 
         models.Study.findByPk(studyId)
@@ -217,7 +216,7 @@ const secretCheckin = (req, res) => {
                                 timeslots[0].attended = true;
                                 timeslots[0].save()
                                     .then(() => res.status(200)
-                                        .json({message: "Timeslot has been marked attended successfully"}));
+                                        .json({message: "Timeslot has been marked attended successfully."}));
                             }
                         });
                 }
