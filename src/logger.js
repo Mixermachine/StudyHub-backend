eba541e010a9776b59f5836c951efa7496883d87
Mixermachine <contact@9d3.de>
2019-07-12 22:37:23 +0200

    - Adding functionality to the logger

--- src/logger.js
+++ src/logger.js
@@ -1,28 +1,35 @@
 const config = require('./config').config['logger'];
 const winston = require('winston');
+const uuidv4 = require('uuid/v4');
+
 
 const logger = winston.createLogger({
     level: config.logLevel,
     format: winston.format.json(),
-    defaultMeta: {service: 'user-service'},
+    defaultMeta: {class: 'base'},
     transports: [
         //
         // - Write to all logs with level `info` and below to `combined.log`
         // - Write all logs error (and below) to `error.log`.
         //
         new winston.transports.File({filename: 'error.log', level: 'error'}),
-        new winston.transports.File({filename: 'combined.log'})
+        new winston.transports.File({filename: 'http.log', level: 'http'}),
+        new winston.transports.File({filename: 'combined.log'}),
+        new winston.transports.Console({format: winston.format.simple()})
     ]
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
+const logRestCall = (req, res, next) => {
+    req.uuid = uuidv4();
+    logger.http(JSON.stringify({uuid: req.uuid, url: req.originalUrl}));
+
+    next();
+};
+
+const wrapMessageWithUuid = (req, msg) => {
+    return JSON.stringify({uuid: req.uuid, message: msg});
+};
+
+logger.logRestCall = logRestCall;
 
 module.exports = logger;
